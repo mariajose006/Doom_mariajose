034e1ac11d8e8f7b763b22eae14199c817c245d7
EggsF0rAll <carol.perez@uteca.edu.mx>
2026-05-21 20:37:49 -0600

    Pistola y animaciones

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   
+   [SerializeField]
+
+   private Transform gunPosition;
+
+ private void OnTriggerEnter(Collider other) 
+ {
+    if (other.CompareTag("Gun"))
+        {
+          other.GetComponent<Gun>().GrabGun(gunPosition);
+   }
+
+}
+
+}
