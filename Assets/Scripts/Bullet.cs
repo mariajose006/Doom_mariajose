c182e4ea1beae535da0afabba682e04c1d190c50
EggsF0rAll <carol.perez@uteca.edu.mx>
2026-05-26 20:36:44 -0600

    agregamos arma y bala junto con interfaz

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+  [SerializeField]
+
+  private float speed = 20f;
+
+  void Awake()
+    {
+        GetComponent<Rigidbody>().linearVelocity = transform.forward * speed;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
@@ -6,12 +7,27 @@ public class Player : MonoBehaviour
    [SerializeField]
 
    private Transform gunPosition;
+   [SerializeField]
+
+   private UnityEvent onGunGrabbed;
+   [SerializeField]
+
+   private UnityEvent onGunDropped;
+
+   private Gun currentGun;
+
+   private void Start()
+   {
+      onGunDropped?.Invoke();
+   }
 
  private void OnTriggerEnter(Collider other) 
  {
     if (other.CompareTag("Gun"))
         {
-          other.GetComponent<Gun>().GrabGun(gunPosition);
+          currentGun = other.GetComponent<Gun>();
+          currentGun.GrabGun(gunPosition);
+          onGunGrabbed?.Invoke();
    }
 
 }
